843ce915838b715611606b7816aec0dca9e66113
Piotr Glodek <glodekpiotr@wp.pl>
2020-08-12 21:14:58 +0200

    added route for Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+// react-router-dom
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <>notedo</>;
+  return (
+    <div className='app-container'>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
