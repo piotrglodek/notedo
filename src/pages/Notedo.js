ef74e5c1ca25bbf29772147a26991f3388d0a44f
Piotr Glodek <glodekpiotr@wp.pl>
2020-08-12 23:48:11 +0200

    added: Tabs, NoNotes, Note, redux logic

--- src/pages/Notedo.js
+++ src/pages/Notedo.js
@@ -1,11 +1,54 @@
 import React from 'react';
 // components
 import { Header } from '../components/Header';
+import { Tabs } from '../components/Tabs';
+import { NoNotes } from '../components/NoNotes';
+// icons
+import { ReactComponent as NotesIconSvg } from '../assets/icons/description_icon.svg';
+import { ReactComponent as FavoriteIconSvg } from '../assets/icons/favorite-filed_icon.svg';
+// redux components
+import { Note } from '../redux/components/Note';
+// redux
+import { useSelector } from 'react-redux';
+// selectors
+import {
+  selectAllNotes,
+  selectFavoriteNotes,
+} from '../redux/features/notesSlice';
 
 export const Notedo = () => {
+  const notes = useSelector(selectAllNotes);
+  const favoriteNotes = useSelector(selectFavoriteNotes);
+  const whatToRender = (data) => {
+    if (data.length !== 0) {
+      return (
+        <>
+          {data.map((data) => (
+            <Note key={data.id} data={data} />
+          ))}
+        </>
+      );
+    } else {
+      return <NoNotes />;
+    }
+  };
   return (
     <>
       <Header />
+      <Tabs
+        tabs={[
+          {
+            label: 'All notes',
+            tabIcon: <NotesIconSvg />,
+            render: whatToRender(notes),
+          },
+          {
+            label: 'Favorite notes',
+            tabIcon: <FavoriteIconSvg />,
+            render: whatToRender(favoriteNotes),
+          },
+        ]}
+      />
     </>
   );
 };
