668f68818de60e571ce0e590485737825adfe086
Piotr Glodek <glodekpiotr@wp.pl>
2020-08-13 00:17:31 +0200

    adding localStorage functions

---
+++ src/localStorage.js
@@ -0,0 +1,22 @@
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return null;
+    }
+    console.log('new pre state', JSON.parse(serializedState));
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+    console.log('saved note to localStorage');
+  } catch (err) {
+    console.log(err);
+  }
+};
