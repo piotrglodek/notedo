039fdeb8878dca647e9d0ad72d58a3ff60b3ea41
Piotr Glodek <pioglodek@gmail.com>
2021-01-04 18:29:54 +0100

    Change header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,38 @@
-import React from 'react';
-// react-router-dom
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button } from './';
 
 export const Header = () => {
   return (
-    <header className='header'>
-      <div className='header__container'>
-        <Link className='header__title' to='/notedo'>
-          Notedo
-        </Link>
-      </div>
-    </header>
+    <StyledHeader>
+      <StyledTitle>Notedo</StyledTitle>
+      <StyledWrapper>
+        <Button variant='secondary' size='small' label='Register' />
+      </StyledWrapper>
+    </StyledHeader>
   );
 };
+
+const StyledHeader = styled.header`
+  padding: 1.2rem 1.6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const StyledTitle = styled.span`
+  display: block;
+  color: ${({ theme: { color } }) => color.black};
+  letter-spacing: 0.25px;
+  font-size: ${({ theme: { fontSize } }) => fontSize.m};
+  font-weight: ${({ theme: { fontWeight } }) => fontWeight.semiBold};
+
+  @media screen and (min-width: 768px) {
+    font-size: ${({ theme: { fontSize } }) => fontSize.l};
+  }
+`;
+
+const StyledWrapper = styled.div`
+  & :first-child {
+    margin-right: 1rem;
+  }
+`;
