31310d56dda67d95aabf07fd67a9da31c5bf44cf
Piotr Glodek <pioglodek@gmail.com>
2021-01-04 18:36:15 +0100

    Add header to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,10 @@
-import React from 'react';
-// react-router-dom
-import { Link } from 'react-router-dom';
-// image
-import notesImageSrc from '../assets/images/notes_image.svg';
+// components
+import { Header } from '../components';
 
 export const Home = () => {
   return (
-    <div className='home'>
-      <div className='home__span'>
-        <h1 className='home__heading'>
-          Notedo will save <br /> Your thoughts
-        </h1>
-        <p className='home__text'>
-          Notedo is tool that allows you to add, edit and delete notes in any
-          time you want.
-        </p>
-        <Link className='btn btn--big btn--filled' to='/notedo'>
-          Try notedo
-        </Link>
-      </div>
-      <div className='home__span'>
-        <img src={notesImageSrc} alt='notes' className='home__image' />
-      </div>
-      <footer className='home__footer'>
-        <a
-          rel='noopener noreferrer'
-          className='home__footer__link'
-          target='_blank'
-          href='https://stories.freepik.com/work'
-        >
-          Illustrations by Freepik
-        </a>
-      </footer>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
