541294022a4d4e4154746bedfdc8d5cb686c2d15
Piotr Glodek <pioglodek@gmail.com>
2021-01-06 11:24:46 +0100

    Add private route and auth state change handling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import { useEffect } from 'react';
 // router
 import { Switch, Route, Redirect } from 'react-router-dom';
 // page
-import { Home } from './pages';
+import { Home, Notedo, PrivateRoute } from './pages';
+// components
+import { Header } from './components';
+// redux
+import { useDispatch } from 'react-redux';
+import { authUser, logOutUser } from './store/reducers/authSlice';
+// firebase
+import { auth } from './firebase';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(authUser());
+      } else {
+        dispatch(logOutUser());
+      }
+    });
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='*'>
-        <Redirect to='/' />
-      </Route>
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <PrivateRoute exact path='/notedo'>
+          <Notedo />
+        </PrivateRoute>
+        <Route exact path='/' component={Home} />
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
