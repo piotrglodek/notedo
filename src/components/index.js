9bd77d7e02065370832469908e9b6849438e47f9
Piotr Glodek <pioglodek@gmail.com>
2021-01-08 20:46:21 +0100

    Reexport CreateNote and Textarea components

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,5 @@ export { Input } from './Input';
 export { Form } from './Form';
 export { Login } from './Login';
 export { Register } from './Register';
+export { CreateNote } from './CreateNote';
+export { Textarea } from './Textarea';
