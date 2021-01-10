4300571446cef19510a24e14f2100e06fd237c93
Piotr Glodek <pioglodek@gmail.com>
2021-01-10 15:11:03 +0100

    Add layout animation to app component

--- src/pages/Notedo.js
+++ src/pages/Notedo.js
@@ -1,10 +1,14 @@
 import { CreateNote, NoteList } from '../components';
+// framer
+import { motion, AnimateSharedLayout } from 'framer-motion';
 
 export const Notedo = () => {
   return (
-    <>
-      <CreateNote />
-      <NoteList />
-    </>
+    <AnimateSharedLayout>
+      <motion.div layout>
+        <CreateNote />
+        <NoteList />
+      </motion.div>
+    </AnimateSharedLayout>
   );
 };
